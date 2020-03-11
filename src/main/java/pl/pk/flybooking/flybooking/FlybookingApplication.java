864c77f11a5900d3ad4d4614021b855fa56351b9
Michał Florczak <michal.florczak@fideltronik.com>
2020-03-11 17:57:46 +0100

    Initial commit

---
+++ src/main/java/pl/pk/flybooking/flybooking/FlybookingApplication.java
@@ -0,0 +1,13 @@
+package pl.pk.flybooking.flybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlybookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlybookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pk/flybooking/flybooking/FlybookingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pk.flybooking.flybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlybookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
